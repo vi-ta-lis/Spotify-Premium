141a09f9debd1db5dc49d345a732a925d2a96594
Vitalis <vitaliskalu7@gmail.com>
2024-08-18 00:30:16 +0100

    added redesign

--- script.js
+++ script.js
@@ -13,7 +13,15 @@
         btnToScroll.addEventListener('click', function(){
             btnToScroll.scrollIntoView({behavior:"smooth"});
         });
+ 
+
+function showSidebar() {
+         const sidebar = document.querySelector('.sidebar')
+                sidebar.style.display = 'flex'
+    }
 
 
-
-       
+    function hideSidebar() {
+         const sidebar = document.querySelector('.sidebar')
+                sidebar.style.display = 'none'
+    }
